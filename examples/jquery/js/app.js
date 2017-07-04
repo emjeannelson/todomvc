2c2e21a6e1dc40dc5ea62800b3e3a5cd2de619e9
Emily Nelson <emjeannelson@gmail.com>
2017-07-04 13:39:56 +0800

    Separate updateStore from render method

--- examples/jquery/js/app.js
+++ examples/jquery/js/app.js
@@ -48,6 +48,7 @@ jQuery(function ($) {
 			new Router({
 				'/:filter': function (filter) {
 					this.filter = filter;
+					this.updateStore();
 					this.render();
 				}.bind(this)
 			}).init('/all');
@@ -63,6 +64,9 @@ jQuery(function ($) {
 				.on('focusout', '.edit', this.update.bind(this))
 				.on('click', '.destroy', this.destroy.bind(this));
 		},
+		updateStore: function() {
+			util.store('todos-jquery', this.todos);
+		},
 		render: function () {
 			var todos = this.getFilteredTodos();
 			$('#todo-list').html(this.todoTemplate(todos));
@@ -70,7 +74,6 @@ jQuery(function ($) {
 			$('#toggle-all').prop('checked', this.getActiveTodos().length === 0);
 			this.renderFooter();
 			$('#new-todo').focus();
-			util.store('todos-jquery', this.todos);
 		},
 		renderFooter: function () {
 			var todoCount = this.todos.length;
@@ -90,7 +93,7 @@ jQuery(function ($) {
 			this.todos.forEach(function (todo) {
 				todo.completed = isChecked;
 			});
-
+			this.updateStore();
 			this.render();
 		},
 		getActiveTodos: function () {
@@ -117,6 +120,7 @@ jQuery(function ($) {
 		destroyCompleted: function () {
 			this.todos = this.getActiveTodos();
 			this.filter = 'all';
+			this.updateStore();
 			this.render();
 		},
 		// accepts an element from inside the `.item` div and
@@ -148,11 +152,13 @@ jQuery(function ($) {
 
 			$input.val('');
 
+			this.updateStore();
 			this.render();
 		},
 		toggle: function (e) {
 			var i = this.indexFromEl(e.target);
 			this.todos[i].completed = !this.todos[i].completed;
+			this.updateStore();
 			this.render();
 		},
 		edit: function (e) {
@@ -184,13 +190,16 @@ jQuery(function ($) {
 				this.todos[this.indexFromEl(el)].title = val;
 			}
 
+			this.updateStore();
 			this.render();
 		},
 		destroy: function (e) {
 			this.todos.splice(this.indexFromEl(e.target), 1);
+			this.updateStore();
 			this.render();
 		}
 	};
 
 	App.init();
 });
+
